8952f6449e17159b706323ad6e1663fa91567bc5
Enrico <randellini@quest-it.com>
2024-11-17 20:31:35 +0100

    minor changes

--- yolo_keypoints_bbox.py
+++ yolo_keypoints_bbox.py
@@ -40,27 +40,27 @@ for idx_person in range(xy.shape[0]):
             annotated_frame_keypoints = cv2.circle(annotated_frame_keypoints, (x,y), radius=3, color=(0, 0, 255), thickness=-1)
             annotated_frame_keypoints = cv2.putText(annotated_frame_keypoints, str(i), (x, y-5), font, 0.7, (0, 0, 255), 2)
 
+    if len(list_x) > 0 and len(list_y) > 0:
+        min_x = min(list_x)
+        max_x = max(list_x)
+        min_y = min(list_y)
+        max_y = max(list_y)
+        print("min_x: {} - max_x: {} - min_y: {} - max_y: {}".format(min_x, max_x, min_y, max_y))
+        w = max_x - min_x
+        h = max_y - min_y
+        dx = int(w/3)
+        x0 = min_x - dx
+        x1 = max_x + dx
+        y0 = min_y - dx
+        y1 = max_y + dx
+        print("x0: {} - x1: {} - y0: {} - y1: {}".format(x0, x1, y0, y1))
 
-    min_x = min(list_x)
-    max_x = max(list_x)
-    min_y = min(list_y)
-    max_y = max(list_y)
-    print("min_x: {} - max_x: {} - min_y: {} - max_y: {}".format(min_x, max_x, min_y, max_y))
-    w = max_x - min_x
-    h = max_y - min_y
-    dx = int(w/3)
-    x0 = min_x - dx
-    x1 = max_x + dx
-    y0 = min_y - dx
-    y1 = max_y + dx
-    print("x0: {} - x1: {} - y0: {} - y1: {}".format(x0, x1, y0, y1))
-
-    coords = (x0, y0 - 10)
-    text = "person"
-    color = (0, 255, 0) # colors in BGR
-    thickness = 3
-    annotated_frame_bbox = cv2.rectangle(annotated_frame_bbox, (x0, y0), (x1, y1), color, thickness)
-    annotated_frame_bbox = cv2.putText(annotated_frame_bbox, text, coords, font, 0.7, color, 2)
+        coords = (x0, y0 - 10)
+        text = "person"
+        color = (0, 255, 0) # colors in BGR
+        thickness = 3
+        annotated_frame_bbox = cv2.rectangle(annotated_frame_bbox, (x0, y0), (x1, y1), color, thickness)
+        annotated_frame_bbox = cv2.putText(annotated_frame_bbox, text, coords, font, 0.7, color, 2)
 
 
 annotated_frame_path = "/home/enrico/Projects/VideoSurveillance/resources/images/annotated_frame_keypoints_900.png"
