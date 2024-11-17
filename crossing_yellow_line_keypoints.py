29f931bdd05c287972e28d5c6305e8dde0125db5
Enrico <randellini@quest-it.com>
2024-11-17 14:38:26 +0100

    minor changes

--- crossing_yellow_line_keypoints.py
+++ crossing_yellow_line_keypoints.py
@@ -198,6 +198,14 @@ if __name__ == '__main__':
                   print("idx_person: {} - is_over: {}".format(idx_person, is_over))
 
           frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+          # resize the frame because of memory
+          # print("frame.shape: ", frame.shape) -> (1920, 1080, 3)
+          new_h = 990
+          aspect_ratio = frame.shape[1]/frame.shape[0]
+          new_w = int(aspect_ratio*new_h)
+          frame = cv2.resize(frame, (new_w, new_h))
+
           frames_list.append(frame)
           count += 1
 
